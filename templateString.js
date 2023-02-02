bd10e31f97b20bf6fd776f7ecf8845fc6125a748
PedroTrudes <pedro.jtrudes@gmail.com>
2023-02-02 16:15:20 -0300

    template Strinh // Ternarios // erros em js

---
+++ templateString.js
@@ -0,0 +1,8 @@
+const nome = "pedro"
+const idade = 2023 - 2000;
+const cidadeNasc= "são paulo"
+
+const apresentacao = `meu nome é ${nome}, minha idade é: ${idade} e nasci em 
+${cidadeNasc}`;
+
+console.log(apresentacao);
---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+if (idadeCliente <= idadeMinima){
+    console.log('Voce não pode beber')
+}else{
+    console.log('voce pode beber')
+}
+
+//rodando com o ternario
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco")
