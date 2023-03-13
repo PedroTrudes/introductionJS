bb5a8c3dee1a6c23f26883a15df2c424d02c1b24
PedroTrudes <pedro.jtrudes@gmail.com>
2023-03-13 18:28:02 -0300

    manipulando uma lista de objetos

---
+++ js-objetos/json/ordenar.js
@@ -0,0 +1,17 @@
+const clientes = require("./clientes.json");
+
+function ordenar(lista, propriedades){
+    const resultado = lista.sort((a,b) => {
+        if (a[propriedades] < b[propriedades]){
+            return -1;
+        } else if(a[propriedades > b[propriedades]]){
+            return 1;
+        }
+        return 0;
+    });
+    return resultado;
+}
+
+const ordenadoPorNome = ordenar(clientes, "nome")
+
+console.log(ordenadoPorNome);
